195f228cac2a12f65526f32e2c8bf24f8e8bcc77
Pacsss <pacayra.justin@gmail.com>
2026-08-31 00:04:14 +0800

    conflict na ata to, pls

--- LoginForm.Designer.cs
+++ LoginForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BillingSystem
 {
-    partial class LoginForm_Conflict
+    partial class LoginForm_Conflictnabapls
     {
         /// <summary>
         /// Required designer variable.
@@ -122,7 +122,7 @@
             label1.TextAlign = ContentAlignment.TopCenter;
             label1.Click += lblTitle_Click;
             // 
-            // LoginForm_Conflict
+            // LoginForm_Conflictnabapls
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
@@ -138,7 +138,7 @@
             Controls.Add(lblTitle);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             MaximizeBox = false;
-            Name = "LoginForm_Conflict";
+            Name = "LoginForm_Conflictnabapls";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Billing System - Login";
             Load += LoginForm_Load;
