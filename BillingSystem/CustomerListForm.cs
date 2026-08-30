5c8ddd6ed31de418678fda10adcb1036036ed4fb
Paolo-Navea <jianpaoloflores.navea25@bicol-u.edu.ph>
2026-08-30 15:45:09 +0800

    AddNewCustumerName

---
+++ BillingSystem/CustomerListForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BillingSystem
+{
+    public partial class CustomerListForm : Form
+    {
+        public CustomerListForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTitle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvCustomers_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void AddCustomerForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
